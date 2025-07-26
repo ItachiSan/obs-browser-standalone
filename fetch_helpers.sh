df0d7f227451ec0c3f24c0e64ddadc0444dbb19a
Giovanni Santini <giovannisantini93@yahoo.it>
2025-07-26 15:51:35 +0200

    feat: 2025 updates
    Rework the helpers files to work with current OBS.
    Requires OBS >= 31.1 for build.

---
+++ fetch_helpers.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+# Dumb script for fetching all the required CMake modules from the main project.
+
+OBS_VERSION="31.1.1"
+
+_myself=$(readlink -f $0)
+_mydir=$(dirname "$_myself")
+
+echo "Working in: $_mydir"
+
+echo "# Preparing work directory..."
+#workdir="$(mktemp -d)"
+workdir="${_mydir}/unpack"
+mkdir -p $workdir
+cd $workdir
+
+if [ ! -f "$OBS_VERSION.tar.gz" ]
+then
+    echo "# Retrieving latest OBS sources..."
+    curl -LO "https://github.com/obsproject/obs-studio/archive/refs/tags/$OBS_VERSION.tar.gz"
+    tar xf "$OBS_VERSION.tar.gz"
+fi
+
+echo "# Merge CMake files..."
+cd "obs-studio-$OBS_VERSION"
+cp -r cmake "${_mydir}"
+
+echo "# Clean up..."
+rm -r $workdir
